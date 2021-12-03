f0ad83ecf2b1c5dd9769128a5740f40473ae7dff
Nick Song <nick.senap@gmail.com>
2021-12-03 17:57:55 +0100

    add day3 correction

--- Day3/3.py
+++ Day3/3.py
@@ -16,24 +16,6 @@ def main():
             gamma += '0'
     a = int(epsilon,2) * int(gamma,2)
     print(a)
-"""
-    if result > 500:
-"""
-def getOxgen(rows, index = 0):
-    if len(rows) == 1:
-        return rows[0]
-    rows = [row for row in rows if row[index] == '1']
-    cols = list(zip(*rows))
-    major = 1
-    if jigSum(cols[index]) > len(cols[index]) / 2:
-        major = 1
-    else:
-        major = 0
-    rows = [row[0] == major for row in rows]
-    getOxgen(rows, index + 1)
-
-
-    
 
 
 if __name__ == '__main__':
