7d71bc204b4eb5761105f02b6de1389f322799b3
Nick Song <nick.senap@gmail.com>
2021-12-09 11:23:53 +0100

    add day 9 p2

--- 2021/Day9/9.py
+++ 2021/Day9/9.py
@@ -1,13 +1,11 @@
+from math import prod
+
+
 def main():
     with open('input.txt') as f:
         data = f.readlines()
-        data = [[int(d) for d in da.strip()] for da in data]
-        result = 0
-        for i in range(len(data)):
-            for j in range(len(data[0])):
-                if is_low_point(data, (i, j)):
-                    result += data[i][j] + 1
-        print(result)
+        print("Part 1:", part1(data))
+        print("Part 2:", part2(data))
 
 
 def is_low_point(data, index):
@@ -40,6 +38,41 @@ def find_pts_around(data, index):
     if y == len(data[0]) - 1:
         return [data[x-1][y], data[x+1][y], data[x][y-1]]
     return [data[x-1][y], data[x+1][y], data[x][y-1], data[x][y+1]]
+
+
+def part1(lines):
+    data = [[int(d) for d in da.strip()] for da in lines]
+    result = 0
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            if is_low_point(data, (i, j)):
+                result += data[i][j] + 1
+    return result
+
+
+def part2(lines):
+    basins = []
+    visited = [[False for _ in line.strip()] for line in lines]
+    for i, line in enumerate(lines):
+        for j, c in enumerate(line.strip()):
+            if visited[i][j] or not ("0" <= c < "9"):
+                continue
+            basin = {(i, j)}
+            stack = [(i, j)]
+            while stack:
+                i2, j2 = stack.pop()
+                visited[i2][j2] = True
+                for i3, j3 in ((i2 - 1, j2), (i2, j2 - 1), (i2, j2 + 1), (i2 + 1, j2)):
+                    if (
+                        0 <= i3 < len(lines)
+                        and 0 <= j3 < len(lines[i3].strip())
+                        and "0" <= lines[i3][j3] < "9"
+                        and (i3, j3) not in basin
+                    ):
+                        basin.add((i3, j3))
+                        stack.append((i3, j3))
+            basins.append(len(basin))
+    return prod(sorted(basins)[-3:])
 
 
 if __name__ == '__main__':
