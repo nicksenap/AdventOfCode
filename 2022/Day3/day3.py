ac1e2e00eae41dd343efe8431dbb4cdaffad39ae
Nick Song <nick.song@klarna.com>
2022-12-03 10:54:49 +0100

    2022 day3

---
+++ 2022/Day3/day3.py
@@ -0,0 +1,48 @@
+def day_3_part_1(imput):
+    SUM = 0
+    for line in input.splitlines():
+        SCORE = 0
+        first, second = split_by_half(line)
+        first_set, second_set = set(first), set(second)
+        match = first_set & second_set
+        if (match):
+            matchStr = "".join(match)
+            matchnum = ord(matchStr) - 96 if matchStr.islower() else (ord(matchStr) - 64) + 26
+            SCORE += matchnum
+        SUM += SCORE
+    return SUM
+        
+def day_3_part_2(input):
+    SUM = 0
+    IDX  = 1
+    buffer = []
+    for line in input.splitlines():
+        buffer.append(line)
+        SCORE = 0
+        if (IDX == 3):
+            IDX = 1
+            a,b,c = buffer
+            buffer = []
+            SCORE += process_input(a,b,c)
+        else:
+            IDX += 1
+        SUM += SCORE
+    return SUM
+
+def process_input(a, b, c):
+    a_set, b_set, c_set = set(a), set(b), set(c)
+    match = a_set & b_set & c_set
+    if (match):
+        matchStr = "".join(match)
+        matchnum = ord(matchStr) - 96 if matchStr.islower() else (ord(matchStr) - 64) + 26
+    return matchnum
+
+
+def split_by_half(input):
+    return input[:len(input)//2], input[len(input)//2:]
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        input = f.read()
+        print(day_3_part_1(input))
+        print(day_3_part_2(input))
